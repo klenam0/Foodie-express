7d5c4568d8786404e01a2c8054abf5a7c895607e
Klenam <cedeegbe@gmail.com>
2024-05-26 19:16:28 +0000

    cart Screen

--- screens/CartScreen.js
+++ screens/CartScreen.js
@@ -1,4 +1,4 @@
-import { View, Text, TouchableOpacity, Image } from "react-native";
+import { View, Text, TouchableOpacity, Image, ScrollView } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 import React from "react";
@@ -44,6 +44,44 @@ export default function CartScreen() {
           </Text>
         </TouchableOpacity>
       </View>
+
+      {/* dishes */}
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{
+          paddingBottom: 50,
+        }}
+        className="bg-white pt-5"
+      >
+        {restaurants.dishes.map((dishes, index) => {
+          return (
+            <View
+              key={index}
+              className="flex-row item-center space-x-3 py-2 px-4 bg-white rounded-3xl mx-2 mb-3 shadow-md"
+            >
+              <Text className="font-bold " style={{ color: themeColors.text }}>
+                2 x
+              </Text>
+              <Image className="h-14 w-14 rounded-full" source={dishes.image} />
+              <Text className="flex-1 font-bold text-gray-700">
+                {dishes.name}
+              </Text>
+              <Text className="font-semibold text-base">${dishes.price}</Text>
+              <TouchableOpacity
+                className="p-1 rounded-full"
+                style={{ backgroundColor: themeColors.bgColor(1) }}
+              >
+                <Icon.Minus
+                  strokeWidth={2}
+                  height={20}
+                  width={20}
+                  stroke="white"
+                />
+              </TouchableOpacity>
+            </View>
+          );
+        })}
+      </ScrollView>
     </SafeAreaView>
   );
 }
