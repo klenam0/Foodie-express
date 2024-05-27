84043fbbb44fac9a71935a311ad3418d4fabf156
Klenam <cedeegbe@gmail.com>
2024-05-27 21:52:27 +0000

    Delivery Screen

--- navigation.js
+++ navigation.js
@@ -8,6 +8,8 @@ import React from "react";
 import HomeScreen from "./screens/HomeScreen";
 import RestaurantScreen from "./screens/RestaurantScreen";
 import CartScreen from "./screens/CartScreen";
+import OrderPreparingScreen from "./screens/OrderPreparingScreen";
+import DeliveryScreen from "./screens/DeliveryScreen";
 
 export default function Navigation() {
   return (
@@ -23,6 +25,16 @@ export default function Navigation() {
           name="Cart"
           options={{ presentation: "modal" }}
           component={CartScreen}
+        />
+        <Stack.Screen
+          name="OrderPreparing"
+          options={{ presentation: "fullScreenModal" }}
+          component={OrderPreparingScreen}
+        />
+        <Stack.Screen
+          name="Delivery"
+          options={{ presentation: "fullScreenModal" }}
+          component={DeliveryScreen}
         />
       </Stack.Navigator>
     </NavigationContainer>
---
+++ screens/OrderPreparingScreen.js
@@ -0,0 +1,21 @@
+import { View, Text, Image } from "react-native";
+import React, { useEffect } from "react";
+import { useNavigation } from "@react-navigation/native";
+
+export default function OrderPreparingScreen() {
+  const navigation = useNavigation();
+  useEffect(() => {
+    setTimeout(() => {
+      // move to delivery screen
+      navigation.navigate("Delivery");
+    }, 3000);
+  }, []);
+  return (
+    <View className="flex-1 bg-white justify-center items-center">
+      <Image
+        source={require("../assets/images/delivery3.gif")}
+        className="h-80 w-80"
+      />
+    </View>
+  );
+}
