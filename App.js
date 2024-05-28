f730844e35c375641e2902944f2b4567596e22b5
Klenam <cedeegbe@gmail.com>
2024-05-28 10:08:39 +0000

    Slices

--- App.js
+++ App.js
@@ -1,7 +1,13 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import Navigation from "./navigation";
+import { Provider } from "react-redux";
+import { store } from "./store";
 
 export default function App() {
-  return <Navigation />;
+  return (
+    <Provider store={store}>
+      <Navigation />
+    </Provider>
+  );
 }
